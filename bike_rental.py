7cf2692c56761cf2fff0c9930ccf73d69dc822de
Konrad Dobros PC <konraddob@gmail.com>
2024-12-01 19:11:09 +0100

    First complications

--- bike_rental.py
+++ bike_rental.py
@@ -3,9 +3,12 @@ from datetime import datetime
 import smtplib
 import os
 
+PATH = "rentals.json"
+
 def rent_bike(customer_name, rental_duration):
     "A function that takes the customer's name and rental time"
     def calculate_cost(rental_duration):
+        "Calculates cost of rental"
         rental_duration = datetime.strptime(rental_duration, "%H:%M").time()
         hours = rental_duration.hour
         minutes = rental_duration.minute
@@ -21,15 +24,27 @@ def rent_bike(customer_name, rental_duration):
     cost = calculate_cost(rental_duration)
     rental = [{"Imie:": customer_name, "Czas:": rental_duration, "Koszt:": cost}]
     def save_rental(rental):
-        path = "rentals.json"
-        if not os.path.exists(path):
-            with open(path, 'w') as file:
-                json.dump(rental, file)
+        "Saves new record to rentals.json"
+        if not os.path.exists(PATH):
+            with open(PATH, 'w') as file:
+                json.dump(rental, file, indent=4)
         else:
-            with open(path, 'a') as file:
+            with open(PATH, 'r+') as file:
                 for rental_entry in rental:
-                    file.write(json.dumps(rental_entry) + "\n")
+                    file.write(json.dumps(rental_entry, indent=4) + "\n")
     save_rental(rental)
 
 rent_bike("Kuba", "1:00")
+
+def load_rentals():
+    "Shows up records from rentals.json"
+    with open(PATH, 'r') as file:
+        rentals = json.load(file)
+        print (rentals)
+
+test = int(input())
+if test == 1:
+    load_rentals()
+        
+
     
